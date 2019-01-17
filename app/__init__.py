9cee23ed47fd58ffc61a446cbed7fff1a128841b
haonle <HaoLe@berkeley.edu>
2019-01-16 21:19:42 -0800

    readded model

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,11 @@ from torchvision import models
 
 app = Flask(__name__)
 
+device = torch.device("cpu")
+classifier = models.resnet18(num_classes=6).to(device)
+classifier.load_state_dict(torch.load('resnet18_1st', map_location='cpu'))
+classifier.eval()
+
 @app.route('/')
 def index():
     return "Hello, World!"
